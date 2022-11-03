be6bd83e0b2d237da8766d3d9cf9789687fae146
Jann Rose Tiongco <jhannprose@gmail.com>
2022-11-04 01:12:46 +0800

    Add views to path

--- SAMS/users/admin.py
+++ SAMS/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Present,Time
 # Register your models here.
+
+admin.site.register(Present)
+admin.site.register(Time)
--- SAMS/users/models.py
+++ SAMS/users/models.py
@@ -5,12 +5,12 @@ import datetime
 # Create your models here.
 
 class Present(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    date = models.DateField(default=datetime.date.today)
+    user = models.ForeignKey(User,blank=False,on_delete=models.CASCADE)
+    date = models.DateField(default=datetime.date.today, blank=False)
     present = models.BooleanField(default=False)
 
 class Time(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField(default=datetime.date.today)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    date = models.DateField(default=datetime.date.today, blank=False)
     time = models.DateTimeField(null=True,blank=True)
     out = models.BooleanField(default=False)
